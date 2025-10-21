04213503956f778e5e7a04383e41a75d361fa299
Patricia Eliseu <patyeliseu4@gmail.com>
2025-10-21 11:52:13 -0300

    add security

---
+++ src/main/java/com/generation/blogPessoal/controller/UsuarioController.java
@@ -0,0 +1,64 @@
+package com.generation.blogPessoal.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogPessoal.model.Usuario;
+import com.generation.blogPessoal.model.UsuarioLogin;
+import com.generation.blogPessoal.service.UsuarioService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(usuarioService.getAllService());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getByIdService(@PathVariable Long id) {
+		return usuarioService.getByIdService(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario usuario) {
+		return usuarioService.cadastrarUsuario(usuario)
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+				.orElse(ResponseEntity.badRequest().build());
+	}
+
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario usuario) {
+		return usuarioService.atualizarUsuario(usuario)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> autenticar(@Valid @RequestBody Optional<UsuarioLogin> usuarioLogin) {
+		return usuarioService.autenticarUsuario(usuarioLogin)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+}
---
+++ src/main/java/com/generation/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.generation.blogPessoal.repository;
+
+
+import java.util.Optional;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogPessoal.model.Usuario;
+ 
+ 
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+ 
+	Optional<Usuario> findByUsuario(String usuario);
+ 
+}
+ 
